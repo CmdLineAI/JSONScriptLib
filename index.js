3df96d8745a99be165bbb6b0bae3f472bcce91d0
Jason Jacobs <nerfnerd@gmail.com>
2024-06-20 23:40:56 -0700

    wonkyness from not resolving promise

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ class JSONScript {
 
       childProcess.on("close", (code) => {
         if (code !== 0) {
-          reject(`Command failed with exit code ${code}`);
+          resolve(`Command failed with exit code ${code}`);
         } else {
           resolve(`Command succeeded with exit code ${code}`);
         }
